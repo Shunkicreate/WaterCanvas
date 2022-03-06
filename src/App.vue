295739f61bfa12a0192188e9ced0805f39716e96
Shunkicreate <you@example.comjmsrsyunrinsyunki@gmail.com>
2022-03-06 19:03:59 +0900

    blur

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
   <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
   <div style="text-align: center;">
     <DrowCanvas></DrowCanvas>
+    <Overlay></Overlay>
   </div>
 </template>
 
@@ -12,11 +13,13 @@ import { defineComponent } from 'vue';
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
 import DrowCanvas from './components/DrowCanvas.vue'
+import Overlay from './components/Overlay.vue'
 export default defineComponent({
   name: 'Home',
   components: {
     HelloWorld,
     DrowCanvas,
+    Overlay,
   }
 })
 
@@ -34,6 +37,7 @@ export default defineComponent({
   margin-top: 60px;
   /* filter: blur(3rem); */
   filter: blur(5px);
+  height: 100%;
 
 }
 </style>
---
+++ src/components/Overlay.vue
@@ -0,0 +1,27 @@
+<template>
+<div>Over Layer</div>
+  <div class="overlay">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Overlay",
+
+});
+</script>
+
+
+<style scoped>
+
+.overlay{
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    filter: blur(5px);
+}
+</style>
