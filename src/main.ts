8030c042a7b22250edf2cf91ad80dfa5731caac7
Shunkicreate <you@example.comjmsrsyunrinsyunki@gmail.com>
2022-03-12 00:24:22 +0900

    remove vuescroll

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import VScrollLock from 'v-scroll-lock'
 // import p5 from '../node_modules/p5/lib/p5.js'
 // import p5 from "p5";
-createApp(App).use(VScrollLock).mount('#app')
+createApp(App).mount('#app')
