12ed2b99168bec845816451ff2db47af55e1e3ab
RyunosukeKurokawa <kuroryu.basket@gmail.com>
2022-03-14 12:06:39 +0900

    タイポ直し

--- src/components/DrawCanvas.vue
+++ src/components/DrawCanvas.vue
@@ -16,7 +16,7 @@ export default defineComponent({
   setup() {
     const positionY = ref(0);
     const positionX = ref(0);
-    // const Drawwidth = ref(window.innerWidth * 0.67);
+    // const DrawWidth = ref(window.innerWidth * 0.67);
     // const DrawHeight = ref(window.innerWidth * 0.67);
     // const HeaderHeight = ref(3)
     // const childDrawCircles: InjectionKey<drawCircles[]> = Symbol('CircleData')
