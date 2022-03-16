5974179915b8c3e96aabeea6b5fd43eb54b1355e
F-mm1229 <meru112mofmof129@gmail.com>
2022-03-16 13:07:09 +0900

    alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from "path";
+// import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  alias: {
-    "/@asseets/": resolve(__dirname, "src/assets"),
-  },
+  // alias: {
+  //   "/@asseets/":path.join(__dirname, "src/assets"),
+  // },
+  // assetsInclude: ['**/*.JPEG', '**/*.png']
+  resolve: {
+    // ↓ 追記
+    alias: {
+      "/@asseets/": `${__dirname}/src/assets/`, // path.join(__dirname, "src/") でも可
+    },
+  }
   // assetsInclude: ['**/*.JPEG', '**/*.png']
 })
